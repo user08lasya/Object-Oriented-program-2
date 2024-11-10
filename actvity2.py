c0b424f66474e5b3fdb9e0e1bef5ed76bb543bfb
user08lasya <lasyapriya0807@gmail.com>
2024-11-10 08:43:38 +0530

    oops-2

--- actvity2.py
+++ actvity2.py
@@ -7,9 +7,21 @@ class Library:
         print(f"We have following books in our library:{self.name}")
         for book in self.booksList:
             print(book)
-    def lendBook(self, user, book):
-        if book not in
+   
+        def lendBook(self, user, book):
+         if book not in self.lendDict.keys():
+             self.lendDict.update({book:user}) 
+             print("Lender-Book database has been updated. You can take the book now")
 
+         else:
+             print(f"Book is already being used by {self.lendDict[book]}")
+
+        def addBook(self, book):
+          self.booksList.append(book) 
+          print("Book has been added to the book list")
+
+          def returnBook (self, book):
+            self.lendDict.pop(book)
 
 
 
